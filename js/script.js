62cd48caa20723bb139f965d92805a8e8bc8ef93
Sar04 <114749685+Sar04@users.noreply.github.com>
2023-06-23 18:40:11 +0300

    remake cell calculation

--- js/script.js
+++ js/script.js
@@ -7,18 +7,28 @@ function initialize(){
         row.classList.add("row");
         for(let x=0;x<16;x++){
             let cell = document.createElement("div");
+            let cellWidth = (CONTAINER.clientWidth - 160)/16;
+            let cellHeight = (CONTAINER.clientHeight - 160)/16;
             cell.classList.add("cell");
+            cell.style.cssText += ` height:${cellHeight}px; width: ${cellWidth}px;`; 
+            if(y === 15){
+                cell.style.cssText += "margin-bottom: 10px;";
+            }
             row.appendChild(cell);
             cell.addEventListener("mouseenter", ()=>{
                 // cell.classList.replace("cell","cell-changed");
                 cell.style.background = "blue";
             });            
         }
+        
         CONTAINER.appendChild(row);
     }
 }
 
 function newGrid(scale){
+    while(CONTAINER.firstChild){
+        CONTAINER.lastChild.remove();
+    }
     for(let y = 0; y<scale;y++){
         let row = document.createElement("div");
         row.classList.add("row");
@@ -29,6 +39,7 @@ function newGrid(scale){
                 cell.style.background = "blue";
             });
         }
+        CONTAINER.appendChild(row);
     }
 }
 
