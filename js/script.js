1d6c43e6c809a55cd4c81b7aa01fc2d0d42bd031
Sar04 <114749685+Sar04@users.noreply.github.com>
2023-06-23 17:48:56 +0300

    add color change functionality

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
 const CONTAINER = document.querySelector(".container");
 
+
 function initialize(){
     for(let y=0;y<16;y++){
         let row = document.createElement("div");
-        console.log(row);
-        row.style.cssText = "display: flex; gap: 10px; background-color: green; margin-bottom: 10px;justify-content: space-evenly; align-items: center;"
+        row.classList.add("row");
         for(let x=0;x<16;x++){
             let cell = document.createElement("div");
-            cell.style.cssText = "height:100px; width: 100px; background-color: red; margin-bottom: 10px; margin-top: 10px"
+            cell.classList.add("cell");
             row.appendChild(cell);
-            
+            cell.addEventListener("mouseenter", ()=>{
+                // cell.classList.replace("cell","cell-changed");
+                cell.style.background = "blue";
+            });            
         }
         CONTAINER.appendChild(row);
     }
 }
 
+function newGrid(scale){
+    for(let y = 0; y<scale;y++){
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let x =0; x<scale; x++){
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.addEventListener("mouseenter", ()=> {
+                cell.style.background = "blue";
+            });
+        }
+    }
+}
+
 initialize();
+
+
 
