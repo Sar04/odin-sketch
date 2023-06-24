673f3b337e1b4a94e80cab470d1d6e8da220cff3
Sar04 <114749685+Sar04@users.noreply.github.com>
2023-06-24 15:11:54 +0300

    add newGrid functionality

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
 const CONTAINER = document.querySelector(".container");
+const NEWGRIDBUTTON = document.querySelector("#newGrid");
 
 
-function initialize(){
-    CONTAINER.style.cssText += `width:${CONTAINER.clientHeight}px;`;
-    for(let y=0;y<16;y++){
+function clearGrid(){
+    while(CONTAINER.firstChild){
+        CONTAINER.lastChild.remove();
+    }
+}
+
+function newGrid(scale){
+    let gap = (scale <60) ? 2:1;
+    for(let y=0;y<scale;y++){
         let row = document.createElement("div");
         row.classList.add("row");
-        for(let x=0;x<16;x++){
+        row.style.cssText += `gap:${gap}px;padding-left:${gap}px;padding-right:${gap}px;`;
+        for(let x=0;x<scale;x++){
             let cell = document.createElement("div");
-            let cellWidth = (CONTAINER.clientWidth - 160)/16;
-            let cellHeight = (CONTAINER.clientHeight - 160)/16;
+            let cellWidth = (CONTAINER.clientWidth - scale*gap)/scale;
+            let cellHeight = (CONTAINER.clientHeight - scale*gap)/scale;
             cell.classList.add("cell");
-            cell.style.cssText += ` height:${cellHeight}px; width: ${cellWidth}px;`; 
-            if(y === 15){
-                cell.style.cssText += "margin-bottom: 10px;";
+            cell.style.cssText += ` height:${cellHeight}px; width: ${cellWidth}px; margin-top:${gap}px`; 
+            if(y === (scale-1)){
+                cell.style.cssText += `margin-bottom:${gap}px;`;
             }
             row.appendChild(cell);
             cell.addEventListener("mouseenter", ()=>{
@@ -24,27 +32,21 @@ function initialize(){
         
         CONTAINER.appendChild(row);
     }
+   
 }
 
-function newGrid(scale){
-    while(CONTAINER.firstChild){
-        CONTAINER.lastChild.remove();
+NEWGRIDBUTTON.addEventListener("click", ()=>{
+    let scale = parseInt(prompt("enter scale of grid (max: 100)", "16"));
+    if(isNaN(scale) || scale === 0 || scale > 100){
+         alert("Invalid number");
+    }else{
+     clearGrid();
+     newGrid(scale);
     }
-    for(let y = 0; y<scale;y++){
-        let row = document.createElement("div");
-        row.classList.add("row");
-        for (let x =0; x<scale; x++){
-            let cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.addEventListener("mouseenter", ()=> {
-                cell.style.background = "blue";
-            });
-        }
-        CONTAINER.appendChild(row);
-    }
-}
+    
+ });
 
-initialize();
+newGrid(16);
 
 
 
